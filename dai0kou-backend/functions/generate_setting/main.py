dd5396decc223f091a52c366cd14e31058e708d2
admin02-at-594175341170 <70833127+atakedemo@users.noreply.github.com>
2025-01-27 12:31:53 +0900

    Firestore更新処理の修正

--- dai0kou-backend/functions/generate_setting/main.py
+++ dai0kou-backend/functions/generate_setting/main.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request
+import uuid
 import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
+from firebase_admin import auth, credentials, firestore
 
 app = Flask(__name__)
 
@@ -9,18 +9,37 @@ cred = credentials.ApplicationDefault()
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
-@app.route('/add', methods=['POST'])
+@app.route('/add_setting', methods=['POST'])
 def add_document():
     try:
+        # auth_header = request.headers.get('Authorization')
         data = request.json
-        collection_name = data.get('collection')
-        document_id = data.get('id')
         document_data = data.get('data')
+        user_id = data.get('user_id')
+        project_id = str(uuid.uuid1())
         
-        if not collection_name or not document_id or not document_data:
-            return jsonify({"error": "collection, id, and data are required"}), 400
-
-        db.collection(collection_name).document(document_id).set(document_data)
+        if not document_data:
+            return jsonify({"error": "data are required"}), 400
+        
+        # Add Project data
+        user_data_ref = db.collection('projects_via_user').document(user_id)
+        user_data_snapshot = user_data_ref.get()
+        if user_data_snapshot.exists:
+            user_data = user_data_snapshot.to_dict()
+            projects_via_user = user_data['projects']
+            projects_via_user.append(project_id)
+            user_data['projects'] = projects_via_user
+            db.collection('projects_via_user').document(user_id).set(user_data)
+        else:
+            user_data = {
+                'user_id': user_id,
+                'projects': [project_id]
+            }
+            db.collection('projects_via_user').document(user_id).set(user_data)
+        
+        # Add Setting
+        db.collection('setting_via_project').document(project_id).set(document_data)
+        
         return jsonify({"message": "Document added successfully"}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
